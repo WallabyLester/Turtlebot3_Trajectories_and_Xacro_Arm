313ab1bdffe9da9463be5e09e20ea0ecbcdef770
Andru Liu <andruliu2022@u.northwestern.edu>
2021-10-13 19:30:02 -0500

    Created launch file and have initial code in trajectory to move.

--- homework2/src/homework2/comp_traj.py
+++ homework2/src/homework2/comp_traj.py
@@ -1,2 +1,8 @@
 """Compute the trajectory and its derivatives at a given time
 """
+import sympy
+from sympy import symbols, sin, cos
+from sympy.abc import v, w
+from math import pi
+
+theta = symbols('theta')
