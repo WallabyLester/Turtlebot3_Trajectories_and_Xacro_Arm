4cd5d055d5fc22f8909c97f1c4f5a25d70d74c96
Andru Liu <andruliu2022@u.northwestern.edu>
2021-10-14 20:20:18 -0500

    Have trajectory reading the python package but it makes a circle.

--- homework2/src/homework2/comp_traj.py
+++ homework2/src/homework2/comp_traj.py
@@ -3,8 +3,8 @@ Compute the trajectory and its derivatives at a given time and
 Compute the proper control inputs at a given time
 """
 
-import sympy as sym
-from sympy import Function, sin, cos, atan2, sqrt
+import sympy
+from sympy import Function, sin, atan2, sqrt, symbols
 from sympy.abc import W, H, T, t
 from math import pi
 
@@ -23,13 +23,24 @@ def comp_traj (W_input, H_input, T_input):
     ydot = y.diff(t)
     xddot = xdot.diff(t)
     yddot = ydot.diff(t)
-    thetadot = theta.diff(t)
 
     # xdot = vcos(theta), ydot = vsin(theta)
     v = sqrt(xdot**2 + ydot**2)
     theta = atan2(ydot, xdot)
+    thetadot = theta.diff(t)
     omega = thetadot
 
+    '''
+    v = v.subs([(W, W_input), (H, H_input), (T, T_input)])
+    omega = omega.subs([(W, W_input), (H, H_input), (T, T_input)])
+    x = x.subs([(W, W_input), (H, H_input), (T, T_input)])
+    xdot = xdot.subs([(W, W_input), (H, H_input), (T, T_input)])
+    xddot = xddot.subs([(W, W_input), (H, H_input), (T, T_input)])
+    y = y.subs([(W, W_input), (H, H_input), (T, T_input)])
+    ydot = ydot.subs([(W, W_input), (H, H_input), (T, T_input)])
+    yddot = yddot.subs([(W, W_input), (H, H_input), (T, T_input)])
+    '''
+   
     list = [v, omega, x, xdot, xddot, y, ydot, yddot]
     values = []
     for i in list:
@@ -45,6 +56,23 @@ def comp_traj (W_input, H_input, T_input):
     ydot = values[6]
     yddot = values[7]
 
-    return x, y, xdot, ydot, xddot, yddot, v, omega
+    # print(omega) for debugging
+    return v, omega, x, xdot, xddot, y, ydot, yddot
 
-comp_traj(10.0, 5.0, 20.0)
+class FigureEight():
+    def __init__(self, W_input, H_input, T_input):
+        self.__x, self.__y, self.__xdot, self.__ydot, self.__xddot, self.__yddot, self.__v, self.__omega = comp_traj(W_input, H_input, T_input)
+        self.__t = symbols('t')  # remember separate from function
+    
+    def get_velocity(self, t):
+        """ Function to return the linear and angular velocities 
+
+        """
+        x = self.__x.subs(self.__t, t)
+        y = self.__y.subs(self.__t, t)
+        v = self.__v.subs(self.__t, t)
+        w = self.__omega.subs(self.__t,t)
+
+        return x, y, v, w
+
+# comp_traj(5, 5, 5) for debugging
