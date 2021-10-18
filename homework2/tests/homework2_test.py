8fc21446647fc024de3acf342eb1528c82c74b8c
Andru Liu <andruliu2022@u.northwestern.edu>
2021-10-18 17:47:33 -0500

    Completed documentation for the test file

--- homework2/tests/homework2_test.py
+++ homework2/tests/homework2_test.py
@@ -1,5 +1,9 @@
-import unittest
+"""
+Tests the python package homework2 calculations 
 
+"""
+
+import unittest
 from numpy.testing._private.utils import assert_almost_equal
 from homework2.comp_traj import FigureEight
 from math import pi, sqrt
@@ -15,15 +19,29 @@ class Test_calc(unittest.TestCase):
         self.figure_eight = FigureEight(W, H, self.T)
 
     def test_time_at_zero(self):
-        """
-        Function to check the calculations at time t = 0
+        """ Function to check the calculations at time t = 0
+
+        Checks the x, y, xdot, ydot, xddot, yddot, v, and omega calculated values 
+
+        Args: 
+            t (time) : 0 
+
+        Returns: 
+            Calculated values and test results 
         """
         assert_almost_equal(self.figure_eight.test(0), (0, 0, 2*pi/3, 2*pi/3, 0, 0, 2*sqrt(2)*pi/3, 0), decimal = 2)
         
 
     def test_time_at_half(self):
-        """
-        Function to check the calculations at time t = T/2 = 3
+        """ Function to check the calculations at half the cycle
+
+        Checks the x, y, xdot, ydot, xddot, yddot, v, and omega calculated values 
+
+        Args: 
+            t (time) : T/2 = 3
+
+        Returns: 
+            Calculated values and test results 
         """
         assert_almost_equal(self.figure_eight.test(self.T/2), (0, 0, -2*pi/3, 2*pi/3, 0, 0, 2*sqrt(2)*pi/3, 0), decimal = 2)
 
